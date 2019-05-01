6ef178be7f240229808e1533d35259d437f50ae0
Alex Mueller <alex@sporkbytes.com>
2019-05-01 16:15:08 -0700

    Changes Rollup to have a single config with multiple outputs.

--- rollup.config.js
+++ rollup.config.js
@@ -2,33 +2,21 @@ import babel from 'rollup-plugin-babel';
 import cleanup from 'rollup-plugin-cleanup';
 import pkg from './package.json';
 
-export default [
-	{
-		input: 'src/index.js',
-		output: {
+export default {
+	input: 'src/index.js',
+	output: [
+		{ file: pkg.main, format: 'cjs' },
+		{ file: pkg.module, format: 'es' },
+		{
 			name: 'mathUtils',
 			file: pkg.umdModule,
 			format: 'umd',
 		},
-		plugins: [
-			babel({
-				exclude: ['node_modules/**'],
-			}),
-			cleanup(),
-		],
-	},
-
-	{
-		input: 'src/index.js',
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' },
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**'],
-			}),
-			cleanup(),
-		],
-	},
-];
+	],
+	plugins: [
+		babel({
+			exclude: ['node_modules/**'],
+		}),
+		cleanup(),
+	],
+};
